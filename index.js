4264430283b8305ff5872c22022d958d89854205
captainalbert <49594557+captainalbert@users.noreply.github.com>
2022-04-24 00:11:43 +0800

    Create a simple API

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const fs = require("fs");
 
-BLOCKING / SYNCHRONOUS;
+// BLOCKING / SYNCHRONOUS;
 const text = fs.readFileSync("./txt/input.txt", "utf-8");
 const newText = `Some avodado description: ${text}`;
 fs.writeFileSync("./txt/output.txt", newText);
 console.log("New file has been created. \nFile name: output.txt");
 
-NON - BLOCKING / ASYNCRHONOUS;
+// NON - BLOCKING / ASYNCRHONOUS;
 fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
   if (err) return console.log("ERROR: ", err);
   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const http = require("http");
-const url = require("url");
+const fs = require("fs");
 
 const port = 8000;
 const address = "127.0.0.2";
+
+const product_data = fs.readFileSync(
+  `${__dirname}/dev-data/data.json`, //'dirname' pertains to the root directory name where this file is location. It is used in replacement of '.'
+  "utf-8"
+);
+const product_data_object = JSON.parse(product_data_object);
 
 // SERVER
 const server = http.createServer((req, res) => {
@@ -12,6 +18,11 @@ const server = http.createServer((req, res) => {
     ? res.end("THIS IS OVERVIEW")
     : route === "/product"
     ? res.end("THIS IS PRODUCT")
+    : route === "/api"
+    ? (res.writeHead(200, {
+        "Content-Type": "application/json",
+      }),
+      res.end(product_data))
     : (res.writeHead(404, {
         "Content-Type": "text/html",
       }),
