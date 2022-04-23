19df5b2cac4e162f89001fa85779d5f703432303
captainalbert <49594557+captainalbert@users.noreply.github.com>
2022-04-23 22:51:36 +0800

    Play around routing and response headers

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const http = require("http");
+const url = require("url");
 
 const port = 8000;
 const address = "127.0.0.2";
 
 // SERVER
 const server = http.createServer((req, res) => {
-  res.end("Hello from the server!");
+  const route = req.url;
+
+  route === "/overview" || route === "/"
+    ? res.end("THIS IS OVERVIEW")
+    : route === "/product"
+    ? res.end("THIS IS PRODUCT")
+    : (res.writeHead(404, {
+        "Content-Type": "text/html",
+      }),
+      res.end("<h1>PAGE NOT FOUND!</h1>"));
 });
 
 server.listen(port, address, () => {
-  console.log(`Server is running on port '${address}:${port}'`);
+  console.log(`Server is running on port 'http://${address}:${port}'`);
 });
